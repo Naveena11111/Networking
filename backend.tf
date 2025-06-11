da65d4737e456ee806d9f387742a84c22e2d1969
Naveena <naveenamohanasundararajan@gmail.com>
2025-06-11 19:15:39 +0100

    test

--- backend.tf
+++ backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket         = "networking-ca1-20065124"
+    bucket         = "networking-ca1-20068144"
     key            = "terraform/terraform.tfstate"
     region         = "eu-north-1"
     encrypt        = true
