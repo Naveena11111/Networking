bf5940669ce11e4d5d9fbb09b7501a33dcf05549
Naveena <naveenamohanasundararajan@gmail.com>
2025-06-29 19:23:49 +0100

    Demo Push

--- main.tf
+++ main.tf
@@ -81,7 +81,7 @@ resource "aws_instance" "web" {
     Name = "Web-Server"
   }
 }
-
+#Demo Testing
 output "public_ip" {
   description = "Public IP of the EC2 instance"
   value       = aws_instance.web.public_ip
